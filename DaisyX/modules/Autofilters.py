3e41f925930685bb731b55fe1e51b8779bb504c7
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-04-04 18:09:37 +0530

    Update Autofilters.py

--- DaisyX/modules/Autofilters.py
+++ DaisyX/modules/Autofilters.py
@@ -9,7 +9,7 @@ Currently support:
      - Music
 
 <b> Setting up </b>
-1) Add @DaisyXBot to your channel
+1) Add @DaisyBeta_Bot to your channel
 2) Make bot admin with full permissions
 2) Go back to your group
 
